201a3c3a36ba44c1f8d6759cedd820b10e3adfb2
samane <samane.ghodrati@gmail.com>
2025-01-20 12:23:16 -0800

    add

---
+++ student-assignments/JS-18-Q4.js
@@ -0,0 +1,8 @@
+/*
+  Write a function that calculates the sum of two numbers.
+
+  - The function must return a promise.
+  - The solution should be solved after half a second.
+  - If a negative value is entered into the function, it will be rejected.
+  - Note: you should use "Promise" to resolve this question.
+*/
---
+++ student-assignments/js-18-q1.js
@@ -0,0 +1,10 @@
+// ? Question-1:follow these steps:
+//todo-1:Write a function called "calculate" that takes 4 parameters.
+//todo-2:The first and seconde parameters should be numbers.
+//todo-3:The third parameter should be a string indicating either "add => addition" or "multiply => multiplication".
+//todo-4:The fourth parameter should be a function that log the result of the calculation.
+
+function calculate() {}
+
+calculate(5, 3, "add", handleCalculationResult);
+calculate(4, 6, "multiply", handleCalculationResult);
